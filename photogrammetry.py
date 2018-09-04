cc1ae36575f5ff48178ccccd1c6b942258be4db9
Stephen Parsons <srpa226@uky.edu>
2018-09-04 13:36:07 -0400

    Initial commit of full pipeline

--- photogrammetry.py
+++ photogrammetry.py
@@ -10,15 +10,50 @@ def main():
     parser.add_argument('output', help='directory for output files')
     args = parser.parse_args()
 
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
+    if not os.path.exists(os.path.join(args.output, 'openMVS')):
+        os.makedirs(os.path.join(args.output, 'openMVS'))
 
-    subprocess.run([
-        'python3',
-        'build/openMVG-prefix/src/openMVG-build/software/SfM/SfM_GlobalPipeline.py',
-        args.input,
-        os.path.join(args.output, 'openMVG')
-    ])
+    commands = [
+        # https://openmvg.readthedocs.io/en/latest/software/SfM/SfM/
+        [
+            'python',
+            'build/openMVG-prefix/src/openMVG-build/software/SfM/SfM_GlobalPipeline.py',
+            args.input,
+            os.path.join(args.output, 'openMVG')
+        ],
+
+        # https://github.com/cdcseacave/openMVS/wiki/Usage
+        [
+            'build/openMVG-prefix/src/openMVG-build/Linux-x86_64-Release/openMVG_main_openMVG2openMVS',
+            '-i', os.path.join(args.output, 'openMVG', 'reconstruction_global', 'sfm_data.bin'),
+            '-o', os.path.join(args.output, 'openMVS', 'scene.mvs'),
+            '-d', os.path.join(args.output, 'openMVG', 'undistorted_images'),
+        ],
+        [
+            'build/openMVS-prefix/src/openMVS-build/bin/DensifyPointCloud',
+            os.path.join(args.output, 'openMVS', 'scene.mvs'),
+            '-w', os.path.join(args.output, 'openMVS', 'working')
+        ],
+        [
+            'build/openMVS-prefix/src/openMVS-build/bin/ReconstructMesh',
+            os.path.join(args.output, 'openMVS', 'scene_dense.mvs'),
+            '-w', os.path.join(args.output, 'openMVS', 'working'),
+        ],
+        [
+            'build/openMVS-prefix/src/openMVS-build/bin/RefineMesh',
+            os.path.join(args.output, 'openMVS', 'scene_dense_mesh.mvs'),
+            '-w', os.path.join(args.output, 'openMVS', 'working'),
+        ],
+        [
+            'build/openMVS-prefix/src/openMVS-build/bin/TextureMesh',
+            os.path.join(args.output, 'openMVS', 'scene_dense_mesh_refine.mvs'),
+            '-w', os.path.join(args.output, 'openMVS', 'working'),
+        ]
+    ]
+
+    for command in commands:
+        print(command)
+        subprocess.run(command)
 
 
 if __name__ == '__main__':
